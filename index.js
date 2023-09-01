ffa2f38499bffd44062597cdc4cb4a796457f292
Tevon Talbert <TevonTalbert@hotmail.com>
2023-09-01 10:57:16 -0500

    adding

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const input = document.getElementById('button');
+const input1 = document.getElementById('click');
 function addingEventListener() {
-}
+   
+    input.addEventListener('click', function () {
+        alert('I was clicked!');
+    });
+
+    }
+
+
